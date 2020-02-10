1ee5142bca523d1b6f4e8d8ec9e6a3c74f848dbd
Jukess <juancaluque@hotmail.es>
2020-02-10 13:15:18 -0600

    Adding CaptureSession

--- VisionApp/Controller/CameraVC.swift
+++ VisionApp/Controller/CameraVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
@@ -18,12 +19,56 @@ class CameraVC: UIViewController {
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var roundedLblView: RoundedShadowView!
     
+    //MARK: VARIABLES
+    var captureSession: AVCaptureSession! //Central hub, real time caption.
+    var cameraOutput: AVCapturePhotoOutput!//Allow camera caption
+    var previewLayer: AVCaptureVideoPreviewLayer!//Shows the camera
     
-    
-    
+    //MARK: LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(cameraOutput) == true {
+                captureSession.addOutput(cameraOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer)
+                captureSession.startRunning()
+            }
+            
+            
+            
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+        
+        
+        
     }
 
 
