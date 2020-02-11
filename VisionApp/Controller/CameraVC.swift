89066b42ac030030a767ce017e1124acb0a8e4cf
Jukess <juancaluque@hotmail.es>
2020-02-11 08:18:26 -0600

    Added CoreML model

--- VisionApp/Controller/CameraVC.swift
+++ VisionApp/Controller/CameraVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
 
 class CameraVC: UIViewController {
 
@@ -23,6 +25,7 @@ class CameraVC: UIViewController {
     var captureSession: AVCaptureSession! //Central hub, real time caption.
     var cameraOutput: AVCapturePhotoOutput!//Allow camera caption
     var previewLayer: AVCaptureVideoPreviewLayer!//Shows the camera
+    var photoData: Data?
     
     //MARK: LIFECYCLE
     override func viewDidLoad() {
@@ -37,6 +40,9 @@ class CameraVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
         
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
@@ -58,19 +64,63 @@ class CameraVC: UIViewController {
                 previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
                 
                 cameraView.layer.addSublayer(previewLayer)
+                cameraView.addGestureRecognizer(tap)
                 captureSession.startRunning()
             }
-            
-            
-            
         } catch {
             debugPrint(error.localizedDescription)
         }
-        
-        
-        
     }
 
-
+    //MARK: @OBJC
+    
+    @objc func didTapCameraView() {
+        let settings = AVCapturePhotoSettings()
+//        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+//        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String: 160]
+//
+//        settings.previewPhotoFormat = previewFormat
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    //MARK: FUNC
+    
+    func resultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        for classification in results {
+            if classification.confidence < 0.5 {
+                self.identificationLbl.text = "Not sure what this is, please try again."
+                self.confidenceLbl.text = ""
+                break
+            } else {
+                self.identificationLbl.text = "\(classification.identifier)"
+                self.confidenceLbl.text = "CONFIDENCE: \(Int(classification.confidence * 100))%"
+                break
+            }
+        }
+    }
 }
 
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error.localizedDescription)
+        } else {
+            photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                try handler.perform([request])
+            } catch {
+                debugPrint(error.localizedDescription)
+            }
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+        }
+    }
+}
